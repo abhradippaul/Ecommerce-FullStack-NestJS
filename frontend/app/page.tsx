39aa48466a72a726a013eb1f2cb592ee26f6461e
Abhradip Paul <abhradipserampore@gmail.com>
2025-11-18 16:09:48 +0530

    Frontend and backend setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl text-red-500">Hello</h1>
+      <Button>Add</Button>
+    </div>
+  );
+}
